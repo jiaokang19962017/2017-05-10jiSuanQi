a638980715fdebdaae2cb6cffaef3b219cc1f2b0
焦康 <Administrator@HP201-1>
2017-05-10 10:59:36 +0800

    那个老师走了???

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,13 +10,68 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入第一个数");
-            int first;
-            first = int.Parse(Console.ReadLine());
-            Console.WriteLine("请输入第二个数");
-            int second;
-            second = int.Parse(Console.ReadLine());
-            Console.WriteLine("{0}加{1}的和是:{2}", first, second, first + second);
+            #region 计算
+            //Console.WriteLine("请输入第一个数");
+            //int first;
+            //first = int.Parse(Console.ReadLine());
+            //Console.WriteLine("请输入第二个数");
+            //int second;
+            //second = int.Parse(Console.ReadLine());
+            //Console.WriteLine("{0}加{1}的和是:{2}", first, second, first + second);
+            #endregion
+            #region 机票
+            //Console.WriteLine("请输入月份");
+            //int month;
+            //month = int.Parse(Console.ReadLine());
+            //Console.WriteLine("请输入价格");
+            //int prise;
+            //prise = int.Parse(Console.ReadLine());
+            //Console.WriteLine("请输入机票类型1/0");
+            //int type = int.Parse(Console.ReadLine());
+            //if (month >= 5 && month <= 10)
+            //{
+            //    if (type == 1)
+            //    {
+            //        Console.WriteLine("价格是{0}", prise * 0.9);
+            //    }
+            //    else if (type == 0)
+            //    {
+            //        Console.WriteLine("价格是{0}", prise * 0.75);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("你输入的类型有误,请重新选择1/0");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("您输入的月份有误,请从新输入");
+            //}
+
+            #endregion
+            int time = int.Parse(Console.ReadLine());
+            switch (time) {
+                case 6:
+                    Console.WriteLine("起床时间");
+                    break;
+                case 7:
+                    Console.WriteLine("吃饭时间");
+                    break;
+                case 8:
+                    Console.WriteLine("上课时间");
+                    break;
+                case 9:
+                case 10:
+                    Console.WriteLine("早上好");
+                    break;
+                default:
+                    Console.WriteLine("请勿打扰");
+                    break;
+
+
+            }
+
+
 
         }
     }
